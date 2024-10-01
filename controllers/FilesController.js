307d464eb3d999510c8b7d062ffd1db3e1841b87
“abdullah <abdullah.h.elgabry@gmail.com>
2024-10-01 15:26:07 +0300

    file_ctr_manger_acc

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -33,9 +33,9 @@ const isValidId = (id) => {
   const size = 24;
   let i = 0;
   const charRanges = [
-    [48, 57], // 0 - 9
-    [97, 102], // a - f
-    [65, 70], // A - F
+    [48, 57],
+    [97, 102],
+    [65, 70],
   ];
   if (typeof id !== 'string' || id.length !== size) {
     return false;
