db9dcfe2c05bd15dc384232c5931142c9de40c35
“abdullah <abdullah.h.elgabry@gmail.com>
2024-10-01 13:11:57 +0300

    file_manger_acc

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,12 +3,17 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 /**
- * AppController class.
- * @class
- * @classdesc Class representing the AppController.
+ * The AppController handles requests related to the application's
+ * overall status and statistics. It provides two main routes:
+ * - `getStatus`: Returns the connection status of Redis and the database.
+ * - `getStats`: Returns the number of users and files in the database.
  */
-
 export default class AppController {
+  /**
+   * Responds with the status of both Redis and the database.
+   * @param {Object} req The request object.
+   * @param {Object} res The response object.
+   */
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
@@ -16,6 +21,11 @@ export default class AppController {
     });
   }
 
+  /**
+   * Retrieves and responds with the total count of users and files from the database.
+   * @param {Object} req The request object.
+   * @param {Object} res The response object.
+   */
   static getStats(req, res) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([usersCount, filesCount]) => {
